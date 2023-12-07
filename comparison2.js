a8f9554a62adb260b47cdeda9332453cd830a505
mwtom4 <malcolm.tom19@gmail.com>
2023-12-07 13:43:05 -0800

    Comparison NULL error update

--- comparison2.js
+++ comparison2.js
@@ -1,31 +1,48 @@
 function compareHeroes(hero1Id, hero2Id) {
-    const hero1 = fetch(`https://superheroapi.com/api.php/ 3328323083897178/${hero1Id}`)
-        .then((response) => response.json())
-    const hero2 = fetch(`https://superheroapi.com/api.php/ 3328323083897178/${hero2Id}`)
-        .then((response) => response.json())
-
-    //fetch hero 1
-
-    return Promise.all([hero1, hero2])
-        .then(([hero1, hero2]) => {
-            console.log(hero1, hero2)
-
-            if (hero1.powerstats.combat > hero2.powerstats.combat) {
-                console.log(`${hero1.name} wins!`);
-                return hero1Id
-            } else if (hero2.powerstats.combat > hero1.powerstats.combat) {
-                console.log(`${hero2.name} wins!`);
-                return hero2Id
-            } else {
-                console.log('It\'s a tie!');
-                //0 doesn't exist as an id, indicates that it's a tie
-                return 0
-            }
-        })
-        .catch(error => {
-            console.log('error loading heroes', error)
-        })
-
-    return
-
-}
+    const fetchHero = async (heroId) => {
+      try {
+        const response = await fetch(`https://superheroapi.com/api.php/3328323083897178/${heroId}`);
+        const data = await response.json();
+        // Convert null values to 0
+        for (const stat in data.powerstats) {
+          if (data.powerstats[stat] === null) {
+            data.powerstats[stat] = 0;
+          }
+        }
+        return data;
+      } catch (error) {
+        console.error(`Error fetching hero data for hero ${heroId}`, error);
+      }
+    };
+  
+    return Promise.all([fetchHero(hero1Id), fetchHero(hero2Id)])
+      .then(([hero1, hero2]) => {
+        console.log(hero1, hero2);
+  
+        // Calculate average powerstats for each hero
+        const averagePowerstat = (hero) => {
+          const totalStats = Object.values(hero.powerstats).reduce((acc, stat) => acc + stat, 0);
+          return totalStats === 0 ? 0 : totalStats / 6; // Avoid division by zero
+        };
+  
+        const averageHero1 = averagePowerstat(hero1);
+        const averageHero2 = averagePowerstat(hero2);
+  
+        if (averageHero1 > averageHero2) {
+          console.log(`${hero1.name} wins!`);
+          return hero1Id;
+        } else if (averageHero2 > averageHero1) {
+          console.log(`${hero2.name} wins!`);
+          return hero2Id;
+        } else {
+          console.log("It's a tie!");
+          return 0; // 0 indicates a tie
+        }
+      })
+      .catch((error) => {
+        console.log('Error loading heroes', error);
+      });
+  }
+  
+  // Example usage:
+  compareHeroes(1, 2);  
